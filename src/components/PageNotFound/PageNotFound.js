694ad6e4327bc7219b87254c4186e2702150d4da
Vera Anyasova <vera.anyasova1@yandex.ru>
2023-07-18 18:28:27 +0500

    перенаправить редирект на '/' при попытке перейти на любой защищённый роут

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -8,7 +8,7 @@ function PageNotFound() {
     <div className="pageNotFound">
       <h2 className="pageNotFound__title">404</h2>
       <p className="pageNotFound__message">Страница не найдена</p>
-      <Link to="/sign-in" className="pageNotFound__link">
+      <Link to="/" className="pageNotFound__link">
         Назад
       </Link>
     </div>
